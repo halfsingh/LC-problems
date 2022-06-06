6b5811db031eea7e518d20b77cfc996f27524d13
halfsingh <vijayksingh.1998@gmail.com>
2022-06-06 09:20:38 +0530

    Added py DSA

---
+++ LI cour/DSA/PY/LL/Node.py
@@ -0,0 +1,15 @@
+
+
+class Node:
+
+    def __init__(self, data, nextNode):
+        self.data = data
+        self.nextNode = nextNode
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, x):
+        self.data = x
+
+# TODO ADd setters and getters for nodes and __toStr__
